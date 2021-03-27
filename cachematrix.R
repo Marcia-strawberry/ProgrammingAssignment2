c53d8f842667cdd46941269c67f68c0e846700f1
Marcia-strawberry <55618373+Marcia-strawberry@users.noreply.github.com>
2021-03-27 23:56:24 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,8 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
 makeCacheMatrix <- function(x = matrix()) {
   # todo error if x is not a matrix
   inv <- NULL
@@ -13,6 +18,7 @@ makeCacheMatrix <- function(x = matrix()) {
        getinverse = getinverse)
 }
 
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
